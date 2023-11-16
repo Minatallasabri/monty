d17b546aea97e1071084eec48fe61bac167c871d
Minatallasabri <minatallasabri3380@gmail.com>
2023-11-16 15:52:38 -0800

    task 5

---
+++ nop.c
@@ -0,0 +1,14 @@
+#include "monty.h"
+
+/**
+  * nop - Doesn’t do anything
+  * @stack: The head of the stack
+  * @line_num: The line on which the error occurred
+  *
+  * Return: Nothing
+  */
+void nop(stack_t **stack, unsigned int line_num)
+{
+	(void) stack;
+	(void) line_num;
+}
