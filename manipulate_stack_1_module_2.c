b0ebbafb4bbdfe53f09a6a06e8675ea949dfb4a5
tazy-dev <tazykaharaba2@gmail.com>
2023-11-17 02:58:17 +0000

    More Tasks

--- manipulate_stack_1_module_2.c
+++ manipulate_stack_1_module_2.c
@@ -7,23 +7,19 @@
  */
 void pchar(stack_t **stack, unsigned int line_num)
 {
+	ops.stk = *stack;
+
 	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't char, stack empty\n", line_num);
-		fclose(ops.file);
-		free(ops.line);
-		free_stack();
-		exit(EXIT_FAILURE);
+		free_all();
 	}
 	if ((*stack)->n < 0 || (*stack)->n > 127)
 	{
 		fprintf(stderr, "L%d: can't char, value out of range\n", line_num);
-		fclose(ops.file);
-		free(ops.line);
-		free_stack();
-		exit(EXIT_FAILURE);
+		free_all();
 	}
-	printf("%c\n", (*stack)->n);
+	printf("%c\n", ops.stk->n);
 }
 
 /**
@@ -55,17 +51,21 @@ void pstr(stack_t **stack, unsigned int line_num)
  */
 void rotl(stack_t **stack, unsigned int line_num)
 {
-	stack_t *tmp = *stack, *next = (*stack);
+	int tmp_num;
+	stack_t *tmp = ops.stk;
 	(void) line_num;
-	if (tmp == NULL || tmp->next == NULL)/*empty or one element stack*/
-		return;
 
-	while (tmp != NULL && tmp->next != NULL)
-		tmp = tmp->next;
-	tmp->next = *stack;
-	(*stack) = next->next;
-	next->next = NULL;
-	next->prev = tmp;
+	ops.stk = *stack;
+	if (ops.stk == NULL || ops.stk->next == NULL)/*empty or one element stack*/
+		return;
+	tmp_num = ops.stk->n;
+	while (ops.stk != NULL && ops.stk->next != NULL)
+	{
+		ops.stk->n = ops.stk->next->n;
+		ops.stk = ops.stk->next;
+	}
+	ops.stk = tmp;
+	ops.stk->n = tmp_num;
 }
 /**
  * rotr - Rotate the stack to the right
